d783d068d6789f4e75a7575f611ebfcb5a9874d0
serrrgi0 <sdegtyar@gmail.com>
2016-02-16 23:40:31 +0200

    add main class

---
+++ Lesson3GitHubTest/src/Shag/Main.java
@@ -0,0 +1,10 @@
+package Shag;
+
+/**
+ * Created by Serrrgi0 on 16.02.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
